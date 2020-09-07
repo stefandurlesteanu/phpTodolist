e1043fcd49725c23b51f056f85c13e3c17b40195
stefandurlesteanu <stefan.durlesteanu@gmail.com>
2020-09-07 06:56:49 +0300

    file validation

--- public/utilities.php
+++ public/utilities.php
@@ -25,3 +25,65 @@ function redirect($url) {
     die();
 }
 
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+
+function checkImage($id)
+{
+    $imgErr = "";
+    $target_dir = "/opt/lampp/htdocs/Todolist/public/img/";
+    $target_file = $target_dir . basename($_FILES["taskImage"]["name"][$id]);
+    $dbName = "public/img/" . basename($_FILES["taskImage"]["name"][$id]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+    // Check if image file is a actual image or fake image
+    if (isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["taskImage"]["tmp_name"][$id]);
+        if ($check !== false) {
+            $uploadOk = 1;
+        } else {
+            $imgErr = "File is not an image. <br>";
+            $uploadOk = 0;
+        }
+    }
+
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        $imgErr .= "Sorry, file already exists.<br>";
+        $uploadOk = 0;
+    }
+
+    // Check file size
+    if ($_FILES["taskImage"]["size"][$id] > 500000) {
+        $imgErr .= "Sorry, your file is too large. <br>";
+        $uploadOk = 0;
+    }
+
+    // Allow certain file formats
+    if ($imageFileType !== "jpg" && $imageFileType !== "png" && $imageFileType !== "jpeg"
+        && $imageFileType !== "gif") {
+        $imgErr .= "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
+        $uploadOk = 0;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk === 0) {
+        $imgErr .= "Sorry, your file was not uploaded.<br>";
+        // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["taskImage"]["tmp_name"][$id], $target_file)) {
+            return $dbName;
+        } else {
+            $imgErr .= "Sorry, there was an error uploading your file. <br>";
+        }
+    }
+    return $imgErr;
+}
+
--- views/add.view.php
+++ views/add.view.php
@@ -2,44 +2,33 @@
 
 <?php
 
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
+$imageErr = '';
 
-
-
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-
-if ($_SERVER["REQUEST_METHOD"] === "POST"){
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $dbData = array();
     $len = count($_POST['taskName']);
     for ($i = 0; $i < $len; $i++) {
-        $dbData[] = array(
-            'taskName' => test_input($_POST['taskName'][$i]),
-            'taskDescription' => test_input($_POST['taskDescription'][$i]),
-            'taskDueDate' => $_POST['taskDueDate'][$i],
-            'taskImage' => $_POST['taskImage'][$i]
-        );
+        $imgValidationResponse = checkImage($i);
+        if (strpos($imgValidationResponse, '/')) {
+            $dbData[] = array(
+                'taskName' => test_input($_POST['taskName'][$i]),
+                'taskDescription' => test_input($_POST['taskDescription'][$i]),
+                'taskDueDate' => $_POST['taskDueDate'][$i],
+                'taskImage' => $imgValidationResponse
+            );
+        } else {
+            $imageErr .= $imgValidationResponse;
+        }
     }
 
-    $jsonData = json_encode($dbData);
+    if (strlen($imageErr) > 0) {
+        $jsonData = json_encode($dbData);
 
 
-    echo '<script type="text/javascript">
-        insertDB(' .$jsonData . ')
+        echo '<script type="text/javascript">
+        insertDB(' . $jsonData . ')
     </script>';
-
-
-//    if (empty($_POST["name"])) {
-//        $nameErr = "Name is required";
-//    } else {
-//        $name = test_input($_POST["name"]);
-//    }
-
+    }
 }
 
 ?>
@@ -47,10 +36,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
 <div class="container" id="addTask" style="margin-top: 3rem;">
     <div style="text-align: right;">
         <button type="button" class="btn btn-success" onclick="addNewTask(); btnView(); event.preventDefault();" style="border-radius: 50%;"><i class="fa fa-plus-square"></i></button>
-        <button type="button" class="btn btn-danger" id="removeTaskBtn" onclick="removeTask(); btnView()" style="border-radius: 50%; display: none;" ><i class="fa fa-minus-square"></i></button>
+        <button type="button" class="btn btn-danger" id="removeTaskBtn" onclick="removeTask(); btnView(); event.preventDefault();" style="border-radius: 50%; display: none;" ><i class="fa fa-minus-square"></i></button>
     </div>
 
-    <form  method="post" action="/Todolist/add"  >
+    <span style="color: red"><?= $imageErr ?></span>
+
+    <form  method="post" action="/Todolist/add"  enctype="multipart/form-data">
         <div id="addForm">
             <fieldset style="border:1px solid #ccc !important; padding: 1em 16px; border-radius: 16px; margin-bottom: 2rem;">
                 <legend>New Task:</legend>
@@ -74,7 +65,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
                 </div>
             </fieldset>
         </div>
-        <input type="submit" value="Add Your TODOs" class="btn btn-block btn-lg btn-outline-dark" style="margin-bottom: 10rem">
+        <input type="submit" name="submit" value="Add Your TODOs" class="btn btn-block btn-lg btn-outline-dark" style="margin-bottom: 10rem">
     </form>
 </div>
 
